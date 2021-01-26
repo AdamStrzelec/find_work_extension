f8837447a27125e437f63db58565ed6bf5e41324
adastr96 <adam19960@op.pl>
2021-01-26 02:12:39 +0100

    bugs fixed

--- popup/searchOffers.js
+++ popup/searchOffers.js
@@ -1,19 +1,19 @@
 const searchBtn = document.getElementById('search');
 const allOffers = {
   pracujpl: [],
-  justjoin: [],
   nofluffjobs: []
 }
 
 searchBtn.addEventListener('click', () => {
     window.open("https://www.pracuj.pl/praca/"+programmingLanquage+";kw/"+locationName+";wp?rd=30","_blank", 'width=1, height=1');
-    // window.open("https://justjoin.it/"+locationName+"/"+programmingLanquage,"_blank", 'width=100, height=100');
-    window.open("https://nofluffjobs.com/pl/jobs/"+locationName+"/"+programmingLanquage,"_blank", 'width=1, height=1');
-    
+    window.open("https://nofluffjobs.com/pl/jobs/"+locationName+"/"+programmingLanquage,"_blank", 'width=100, height=100');
 });
+
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       allOffers[request.pageName] = request.offers;
       console.log(allOffers)
+      sendResponse({info: 'data fetched'});
     }
 );
